56a455fdd40451c7deb3862fcb99feaf1fdbc236
Lisa Harrison <lmharrison0810@gmail.com>
2020-12-13 15:40:24 -0500

    Added death map to homepage

--- death_map.py
+++ death_map.py
@@ -10,16 +10,16 @@ cov_cases = pd.read_csv("data/cov_cases.csv")
 fig = go.Figure()
 
 fig = px.choropleth(cov_cases,
-                   color_continuous_scale = 'pubu',
+                   color_continuous_scale = 'rdpu',
                    locations='abbrev',
-                   color='positive',
+                   color='death',
                    hover_name = 'state',
                    locationmode = 'USA-states',
                    animation_frame = 'date')
 
 layout = (
 fig.update_layout(
-title_text = 'Positive Covid-19 cases in the United States',
+title_text = 'Covid-19 Deaths in the United States',
 title_x = 0.5,
 geo_scope = 'usa',
 geo=dict(
@@ -28,7 +28,7 @@ showcoastlines = False))
 )
 
 pio.write_html(fig,
-               file='pos9_cases.html',
+               file='death_cases.html',
                config={'displayModeBar': False},
                auto_open=True,
                include_plotlyjs='cdn',
